199282a672a4dbca5b81c4ba057f19284dd8986c
Marinerbyte <yasinkarani14@gmail.com>
2026-01-12 21:22:23 +0530

    Update text.js

--- utils/text.js
+++ utils/text.js
@@ -1,23 +1,7 @@
-const { newPage } = require("../puppeteer");
+const { searchText } = require("./search");
 
-/**
- * getText(query)
- * - simple info / wiki / news / quiz text fetch
- */
 async function getText(query) {
-  const page = await newPage();
-  try {
-    await page.goto(`https://www.google.com/search?q=${encodeURIComponent(query)}`);
-    const content = await page.evaluate(() => {
-      return document.body.innerText.slice(0, 3000); // max 3000 chars
-    });
-    return content;
-  } catch (err) {
-    console.error("Text fetch error:", err.message);
-    return `No results found for: ${query}`;
-  } finally {
-    await page.close();
-  }
+  return await searchText(query);
 }
 
 module.exports = { getText };
