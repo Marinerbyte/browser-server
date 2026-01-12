859c3e124db57fa60b786e55a8aee58aa0278b53
Marinerbyte <yasinkarani14@gmail.com>
2026-01-12 21:56:23 +0530

    Update puppeteer.js

--- utils/puppeteer.js
+++ utils/puppeteer.js
@@ -1,29 +1,11 @@
-const puppeteer = require("puppeteer-core");
+const puppeteer = require("puppeteer");
 
-let browser;
-
-async function getBrowser() {
-  if (!browser) {
-    browser = await puppeteer.launch({
-      executablePath: "/usr/bin/google-chrome-stable",
-      headless: true,
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage",
-        "--disable-gpu",
-        "--window-size=1920,1080"
-      ]
+async function launchBrowser() {
+    return await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        executablePath: puppeteer.executablePath() // <- Render free tier ke liye correct Chromium path
     });
-  }
-  return browser;
 }
 
-async function newPage() {
-  const b = await getBrowser();
-  const page = await b.newPage();
-  await page.setViewport({ width: 1920, height: 1080 });
-  return page;
-}
-
-module.exports = { newPage };
+module.exports = launchBrowser;
