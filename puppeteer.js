2d3a60528eed8d24066ad5bbff43498f0a6b6f6d
Marinerbyte <yasinkarani14@gmail.com>
2026-01-12 20:15:48 +0530

    Update puppeteer.js

--- puppeteer.js
+++ puppeteer.js
@@ -1,16 +1,20 @@
 const puppeteer = require("puppeteer");
 
-async function withBrowser(fn) {
-  const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
-  });
+let browser;
 
-  try {
-    const page = await browser.newPage();
-    return await fn(page);
-  } finally {
-    await browser.close();
+async function getBrowser() {
+  if (!browser) {
+    browser = await puppeteer.launch({
+      headless: "new",
+      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    });
   }
+  return browser;
 }
 
-module.exports = { withBrowser };
+async function newPage() {
+  const b = await getBrowser();
+  return await b.newPage();
+}
+
+module.exports = { newPage };
