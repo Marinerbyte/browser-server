59daf2cc03537138f305ea75b384578f1d053942
Marinerbyte <yasinkarani14@gmail.com>
2026-01-12 21:21:57 +0530

    Update image.js

--- utils/image.js
+++ utils/image.js
@@ -1,38 +1,8 @@
-const { newPage } = require("../puppeteer");
-const downloader = require("./downloader");
+const { searchImage } = require("./search");
 
-/**
- * getImage(query)
- * - searches image
- * - downloads to /tmp
- * - returns { file, title }
- */
 async function getImage(query) {
-  const page = await newPage();
-  try {
-    await page.goto(`https://www.google.com/search?tbm=isch&q=${encodeURIComponent(query)}`);
-    const url = await page.evaluate(() => {
-      const img = document.querySelector("img");
-      return img ? img.src : null;
-    });
-
-    if (!url) throw new Error("No image found");
-
-    const filePath = await downloader.downloadFromUrl(url, "jpg");
-
-    return {
-      file: filePath,
-      title: query
-    };
-  } catch (err) {
-    console.error("Image fetch error:", err.message);
-    return {
-      file: null,
-      title: query
-    };
-  } finally {
-    await page.close();
-  }
+  const result = await searchImage(query);
+  return result;
 }
 
 module.exports = { getImage };
