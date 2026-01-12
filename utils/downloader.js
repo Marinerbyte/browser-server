396a9631401b47ab30a895bdff2662feb0251b18
Marinerbyte <yasinkarani14@gmail.com>
2026-01-12 22:06:20 +0530

    Update downloader.js

--- utils/downloader.js
+++ utils/downloader.js
@@ -1,17 +1,21 @@
 const fs = require("fs");
-const fetch = require("node-fetch");
 const path = require("path");
-const { v4: uuidv4 } = require("uuid");
 
-async function downloadFromUrl(url, ext = "tmp") {
-  const res = await fetch(url);
-  if (!res.ok) throw new Error("Download failed");
-
-  const buffer = await res.arrayBuffer();
-  const filename = `${uuidv4()}.${ext}`;
-  const filepath = path.join("/tmp", filename);
-  fs.writeFileSync(filepath, Buffer.from(buffer));
-  return filepath;
+async function downloadAudio(query) {
+    const fileName = query.replace(/\s+/g, "_") + ".mp3";
+    const filePath = path.join(__dirname, "..", "downloads", fileName);
+    fs.mkdirSync(path.join(__dirname, "..", "downloads"), { recursive: true });
+    // Placeholder: write empty file, later Puppeteer scrape YouTube / API
+    fs.writeFileSync(filePath, ""); 
+    return filePath;
 }
 
-module.exports = { downloadFromUrl };
+async function downloadImage(query) {
+    const fileName = query.replace(/\s+/g, "_") + ".png";
+    const filePath = path.join(__dirname, "..", "downloads", fileName);
+    fs.mkdirSync(path.join(__dirname, "..", "downloads"), { recursive: true });
+    fs.writeFileSync(filePath, ""); 
+    return filePath;
+}
+
+module.exports = { downloadAudio, downloadImage };
