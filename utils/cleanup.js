1ddb3791ea2f7a7aef43ef6a3e1d69a41ac17a58
Marinerbyte <yasinkarani14@gmail.com>
2026-01-12 22:06:47 +0530

    Update cleanup.js

--- utils/cleanup.js
+++ utils/cleanup.js
@@ -1,21 +1,14 @@
 const fs = require("fs");
 const path = require("path");
 
-const TMP_DIR = "/tmp";
-
-function cleanup() {
-  fs.readdir(TMP_DIR, (err, files) => {
-    if (err) return;
-    files.forEach(file => {
-      const filePath = path.join(TMP_DIR, file);
-      fs.stat(filePath, (err, stats) => {
-        if (err) return;
-        const age = Date.now() - stats.mtimeMs;
-        if (age > 5 * 60 * 1000) fs.unlink(filePath, () => {});
-      });
-    });
-  });
+async function cleanupFiles() {
+    const dir = path.join(__dirname, "..", "downloads");
+    if (!fs.existsSync(dir)) return;
+    const files = fs.readdirSync(dir);
+    for (const f of files) {
+        fs.unlinkSync(path.join(dir, f));
+    }
+    console.log("Cleaned downloads folder");
 }
 
-// Run every 5 minutes
-setInterval(cleanup, 5 * 60 * 1000);
+module.exports = { cleanupFiles };
