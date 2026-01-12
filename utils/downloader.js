4b81a2c882da1a1662e14d30b52f4b7df62f24d7
Marinerbyte <yasinkarani14@gmail.com>
2026-01-12 22:28:37 +0530

    Update downloader.js

--- utils/downloader.js
+++ utils/downloader.js
@@ -3,18 +3,19 @@ const path = require("path");
 
 async function downloadAudio(query) {
     const fileName = query.replace(/\s+/g, "_") + ".mp3";
-    const filePath = path.join(__dirname, "..", "downloads", fileName);
-    fs.mkdirSync(path.join(__dirname, "..", "downloads"), { recursive: true });
-    // Placeholder: write empty file, later Puppeteer scrape YouTube / API
-    fs.writeFileSync(filePath, ""); 
+    const dir = path.join(__dirname, "..", "downloads");
+    fs.mkdirSync(dir, { recursive: true });
+    const filePath = path.join(dir, fileName);
+    fs.writeFileSync(filePath, ""); // placeholder, Puppeteer download implement kar sakte ho
     return filePath;
 }
 
 async function downloadImage(query) {
     const fileName = query.replace(/\s+/g, "_") + ".png";
-    const filePath = path.join(__dirname, "..", "downloads", fileName);
-    fs.mkdirSync(path.join(__dirname, "..", "downloads"), { recursive: true });
-    fs.writeFileSync(filePath, ""); 
+    const dir = path.join(__dirname, "..", "downloads");
+    fs.mkdirSync(dir, { recursive: true });
+    const filePath = path.join(dir, fileName);
+    fs.writeFileSync(filePath, ""); // placeholder
     return filePath;
 }
 
