9a3369c26934f1283b6c98e1ff0d2f833054474b
Marinerbyte <yasinkarani14@gmail.com>
2026-01-12 21:10:14 +0530

    Create puppeteer.js

---
+++ utils/puppeteer.js
@@ -0,0 +1,29 @@
+const puppeteer = require("puppeteer-core");
+
+let browser;
+
+async function getBrowser() {
+  if (!browser) {
+    browser = await puppeteer.launch({
+      executablePath: "/usr/bin/google-chrome-stable", // Render Chrome path
+      headless: true,
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-gpu",
+        "--window-size=1920,1080"
+      ]
+    });
+  }
+  return browser;
+}
+
+async function newPage() {
+  const b = await getBrowser();
+  const page = await b.newPage();
+  await page.setViewport({ width: 1920, height: 1080 });
+  return page;
+}
+
+module.exports = { newPage };
