4bed4ea9a594e2f9f4a2e77942ba33b5266a917f
Marinerbyte <yasinkarani14@gmail.com>
2026-01-12 22:37:25 +0530

    Update puppeteer.js

--- puppeteer.js
+++ puppeteer.js
@@ -3,8 +3,13 @@ const puppeteer = require("puppeteer");
 async function launchBrowser() {
     const browser = await puppeteer.launch({
         headless: true,
-        args: ["--no-sandbox", "--disable-setuid-sandbox"],
-        executablePath: puppeteer.executablePath() // Render free tier compatible
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage",
+            "--disable-gpu"
+        ],
+        executablePath: puppeteer.executablePath() // default Chromium
     });
     return browser;
 }
